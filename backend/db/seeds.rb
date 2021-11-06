a3ea49592665eac5c4a06601eb6fa81e1ef2d9d2
namaba <one.2426@gmail.com>
2021-11-06 11:11:58 +0900

    railsとNext連携

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Post.create!(
+  [
+    {
+      title: 'Next.js + Ruby on Rails + Docker の環境構築'
+    },
+    {
+      title: 'React Hooks でカスタムフックを作る'
+    },
+    {
+      title: 'GraphQL と Apollo Client 入門'
+    },
+    {
+      title: '【TypeScript4.3】Template Literal Types'
+    },
+    {
+      title: 'Tailwind CSS でダークモード実装'
+    },
+  ]
+)
